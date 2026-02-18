c37cb1374bb779ef7a903c2fe2643095d493a817
Saif Metkari <saifmetkari17@gmail.com>
2026-02-18 23:57:11 +0530

    Post Creation API in progress

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.PostsDTO;
+import com.example.demo.dto.PostsRequest;
+import com.example.demo.service.PostService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
-    
+    private final PostService postService;
+
+    @PostMapping("/create-post")
+    public ResponseEntity<PostsDTO> createPost(@RequestBody PostsRequest postsRequest){
+        return new ResponseEntity<PostsDTO>(postService.createPost(postsRequest),HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/demo/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PostServiceImpl.java
@@ -7,10 +7,15 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.PostsDTO;
 import com.example.demo.dto.PostsRequest;
+import com.example.demo.entity.Posts;
 import com.example.demo.entity.User;
+import com.example.demo.exception.FileUploadException;
+// import com.example.demo.helper.SupabaseStorageService;
+import com.example.demo.repository.PostRepository;
 import com.example.demo.security.UserPrincipal;
 import com.example.demo.service.PostService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,12 +23,29 @@ import lombok.RequiredArgsConstructor;
 public class PostServiceImpl implements PostService {
 
     private final ModelMapper modelMapper;
+    private final PostRepository postRepository;
+    // private final SupabaseStorageService supabaseStorageService;
 
     @Override
+    @Transactional
     public PostsDTO createPost(PostsRequest postsRequest) {
         // TODO Auto-generated method stub
         User user = UserPrincipal.getCurrentUser();
-        return null;
+
+        Posts posts = modelMapper.map(postsRequest , Posts.class);
+        posts.setUser(user);
+        // try{
+            // String postImageUrl = supabaseStorageService.uploadProfilePhoto(postsRequest.getImage());
+            String postImageUrl = "URL_COMES_HERE";
+            posts.setImageurl(postImageUrl);
+
+        // }catch (IOException e) {
+            // throw new FileUploadException("Failed to upload posts image: " + e.getMessage(), e);
+        // }
+
+        Posts savedPost = postRepository.save(posts);
+
+        return modelMapper.map(savedPost , PostsDTO.class);
     }
 
     @Override
