60ad54450356237a1c7f35ed7f450e15e055f233
Saif Metkari <88455816+Wiz17@users.noreply.github.com>
2026-02-25 23:46:53 +0530

    Add constructor to ProfileAlreadyExistsException

--- src/main/java/com/example/demo/exception/ProfileAlreadyExistsException.java
+++ src/main/java/com/example/demo/exception/ProfileAlreadyExistsException.java
@@ -2,6 +2,8 @@ package com.example.demo.exception;
 
 public class ProfileAlreadyExistsException extends RuntimeException {
     public ProfileAlreadyExistsException(String message) {
+        
         super(message);
+        
     }
 }
